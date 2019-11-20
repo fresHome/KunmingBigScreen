16cbffe40087a51d1f16694cf4afaa2d597be927
shenssss <s651489571@163.com>
2019-11-20 18:50:26 +0800

    第2

--- src/components/box/BoxTwo.vue
+++ src/components/box/BoxTwo.vue
@@ -18,20 +18,15 @@ export default {
     return {
       option: {
         legend: {
-          bottom: '0',
+          bottom: 'bottom',
           left: 'center',
-          right: '0',
-          width: '70%',
-          orient: 'horizontal',//???
-          itemGap: convertRem(0.4),
           itemWidth: convertRem(0.075),
           itemHeight: convertRem(0.075),
           textStyle: {
             fontSize: convertRem(0.075),
             color: '#B5BDDB'
           },
-          data: ["生物医药产业", "生物医药产业占比", "新材料及装备制造产业", "新材料及装备制造产业占比", "IT及现代服务产业", "IT及现代服务产业占比",
-          ]
+          data: ['生物医药产业', '新材料及装备制造产业', 'IT及现代服务产业']
         },
         grid: {
           left: '20%'
@@ -39,11 +34,9 @@ export default {
         color: ['#7EF7FF', '#6D91FF', '#D7087E'],
         series: [
           {
-            color: ['#32F0FE', '#D7087E', '#3D6CFC'],
             type: 'pie',
-            center: ['25%', '50%'],
+            center: ['37%', '50%'],
             radius: [convertRem(0.15), convertRem(0.35)],
-//            radius: ['4.17%', '100%'],
             label: {
               normal: {
                 show: false
@@ -53,7 +46,7 @@ export default {
           },
           {
             type: 'pie',
-            center: ['25%', '50%'],
+            center: ['37%', '50%'],
             radius: [convertRem(0.4075), convertRem(0.4125)],
             data: [100],
             label: {
@@ -61,9 +54,8 @@ export default {
             }
           },
           {
-            color: ['#32F0FE', '#D7087E', '#3D6CFC'],
             type: 'pie',
-            center: ['75%', '50%'],
+            center: ['63%', '50%'],
             radius: [convertRem(0.15), convertRem(0.35)],
             label: {
               normal: {
@@ -74,7 +66,7 @@ export default {
           },
           {
             type: 'pie',
-            center: ['75%', '50%'],
+            center: ['63%', '50%'],
             radius: [convertRem(0.4075), convertRem(0.4125)],
             data: [100],
             label: {
@@ -82,7 +74,7 @@ export default {
             }
           }
         ]
-      },
+      }
     }
   },
   props: ['delayShow'],
@@ -91,46 +83,32 @@ export default {
       this.activeJJqs = type
       let newOption = deepClone(this.option)
       request.normalPort({
-        codeArray: ['Xh00007', 'Xh00008', 'Xh00009', 'Xh00010', 'Xh00011', 'Xh00012', 'Xh00013', 'Xh00014', 'Xh00015', 'Xh00016', 'Xh00017', 'Xh00018']
+        codeArray: ['Xh00007', 'Xh00009', 'Xh00011', 'Xh00013', 'Xh00015', 'Xh00017']
       }).then(res => {
         let arr1 = [
           { name: '生物医药产业', value: 0 },
-          { name: '生物医药产业占比', value: 0 },
           { name: '新材料及装备制造产业', value: 0 },
-          { name: '新材料及装备制造产业占比', value: 0 },
           { name: 'IT及现代服务产业', value: 0 }
         ]
         let arr2 = [
           { name: '生物医药产业', value: 0 },
-          { name: '生物医药产业占比', value: 0 },
           { name: '新材料及装备制造产业', value: 0 },
-          { name: '新材料及装备制造产业占比', value: 0 },
           { name: 'IT及现代服务产业', value: 0 }
         ]
-        let arrtest1 = 0;
-        let arrtest2 = 0;
-        res.data.data.resultList.map((item, index, arry) => {
-          if (item.code.slice(-2) < 13) {
-            arr1[index] = item.value;
-            arrtest1 = 1;
-          } else {
-            arr2[index] = item.value;
-            arrtest2 = 1;
+        let data1 = res.data.data.resultList.slice(0, 3)
+        let data2 = res.data.data.resultList.slice(3)
+        data1.map((item, index, arry) => {
+          if (item.codeRemark.indexOf(arr1[index].name) >= 0) {
+            arr1[index].value = item.value
           }
         })
-
-        if (arrtest1 == 0) {
-          newOption.series[1].data = []
-          newOption.series[0].data = []
-        } else {
-          newOption.series[0].data = arr1
-        }
-        if (arrtest2 == 0) {
-          newOption.series[3].data = []
-          newOption.series[2].data = []
-        } else {
-          newOption.series[2].data = arr2
-        }
+        data2.map((item, index, arry) => {
+          if (item.codeRemark.indexOf(arr2[index].name) >= 0) {
+            arr2[index].value = item.value
+          }
+        })
+        newOption.series[0].data = arr1
+        newOption.series[2].data = arr2
 
         this.option = newOption
       })
