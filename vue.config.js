518036598a4af0822e927fca71afadcf4e10be92
shenssss <s651489571@163.com>
2019-11-24 23:38:21 +0800

    打包参数修改

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,18 @@
 module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
   assetsDir: 'static',
-  publicPath: '././',
-  devServer: {
-    proxy: {
-      // proxy all requests starting with /api to jsonplaceholder
-      '/api': {
-        target: 'http://121.40.133.115/',   //代理接口
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''    //代理的路径
-        }
-      }
-    }
-  }
+  productionSourceMap: false,
+//  devServer: {
+//    proxy: {
+//      // proxy all requests starting with /api to jsonplaceholder
+//      '/api': {
+//        target: 'http://121.40.133.115/',   //代理接口
+//        changeOrigin: true,
+//        pathRewrite: {
+//          '^/api': ''    //代理的路径
+//        }
+//      }
+//    }
+//  }
 }
