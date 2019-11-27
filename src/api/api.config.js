4b312e4d945de48444c30d012aa197dda9367350
shenssss <s651489571@163.com>
2019-11-27 19:59:23 +0800

    打包+动画

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -2,6 +2,5 @@ import axios from 'axios'
 
 // //axios配置
 axios.defaults.method = 'post';
-axios.defaults.baseURL = '/api/';
 axios.defaults.timeout = 600000;
 axios.defaults.responseType = 'json';
--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,16 @@ module.exports = {
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false,
-//  devServer: {
-//    proxy: {
-//      // proxy all requests starting with /api to jsonplaceholder
-//      '/api': {
-//        target: 'http://121.40.133.115/',   //代理接口
-//        changeOrigin: true,
-//        pathRewrite: {
-//          '^/api': ''    //代理的路径
-//        }
-//      }
-//    }
-//  }
+  devServer: {
+    proxy: {
+      // proxy all requests starting with /api to jsonplaceholder
+      '/api': {
+        target: 'http://121.40.133.115/',   //代理接口
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''    //代理的路径
+        }
+      }
+    }
+  }
 }
